8bb9356d8055b8c9c1cacdeabc4427c0bdc13aca
Sávio Pereira <60441128+savio-fju@users.noreply.github.com>
2023-04-11 23:36:20 -0300

    refatoração do codigo

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
 const express = require('express');
 const server = express();
 
+const ideas = [
+  {
+    img: "",
+    title: "",
+    category: "",
+    description: "",
+    url: ""
+
+  }
+]
+
 server.use(express.static("public"))
 
-server.get('/', function(req, res) {
-  return res.sendFile(__dirname + '/index.html');
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
 })
 
-server.get('/ideas', function(req, res) {
-  return res.sendFile(__dirname + '/ideas.html');
+server.get('/', function (req, res) {
+
+  const reverseIdeas = [...ideas].reverse()
+
+  let lastIdeas = []
+  for (let idea of reverseIdeas) {
+    if (lastIdeas.length < 2) {
+      lastIdeas.push(idea)
+    }
+  }
+
+  return res.render('index.html', { ideas: lastIdeas });
+})
+
+server.get('/ideas', function (req, res) {
+
+  const reverseIdeas = [...ideas].reverse()
+
+  return res.render('ideas.html', { ideas: reverseIdeas});
 })
 
 server.listen(3000)
